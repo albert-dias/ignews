e7d4d4bd58c3bf7d656ad5f4d073f457587e8b9f
Albert Dias <albert.uft@gmail.com>
2023-02-07 20:46:29 -0300

    finish home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -6,16 +7,17 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>👋 Home | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <span>Hey, welcome</span>
+          <span>👋 Hey, welcome</span>
           <h1>News about the <span>React</span> world.</h1>
           <p>
             Get access to all the publication <br />
             <span>for $9.99 month</span>
           </p>
+          <SubscribeButton />
         </section>
         <img src="/images/avatar.svg" alt="Girl codding" />
       </main>
