0423118a514376b2e7faa33289da6c194808fe11
Albert Dias <albert.uft@gmail.com>
2023-04-07 20:27:13 -0300

    fauna integration

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,47 @@
+import { signIn, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
+import { api } from '@/src/services/api';
+import { getStripeJs } from '@/src/services/stripe-js';
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes 
+/**
+ * 
+ * Onde acesso o backend
+ */
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data } = useSession();
+
+  async function handleSubscribe() {
+    if (!data) {
+      signIn('github');
+      return;
+    }
+
+    // CRIAÇÃO Checkou session
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionID } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId: sessionID })
+    } catch (err: any) {
+      alert(err.message);
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { fauna } from '../../../services/fauna';
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
 export const authOptions = {
   providers: [
     GithubProvider({
@@ -8,5 +12,40 @@ export const authOptions = {
       scope: "read.user"
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  }
 }
 export default NextAuth(authOptions)
